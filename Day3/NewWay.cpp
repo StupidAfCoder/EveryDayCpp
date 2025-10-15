625b2889a0965374acd44e9d435fd4bd55478640
Yash Malik <132210135+StupidAfCoder@users.noreply.github.com>
2025-10-16 01:19:38 +0530

    Some learning about pointers

--- Day3/NewWay.cpp
+++ Day3/NewWay.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "log.h"
 
+#define PI 3.14159
+
 int main() {
     InitLog();
     log("Hello world!");
+    std::cout << "Value Of PI: " << PI << std::endl;
 
     return 0;
 }
---
+++ Day3/pointers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define LOG(x) std::cout << x << std::endl;
+
+int main(){
+    int var = 54;
+    LOG(var);
+    int* pointer = &var;
+    *pointer = 65;
+    LOG(var);
+
+    //Sets The values of 0 to the bytes location of the memory address
+    char* buffer = new char[9];
+    memset(buffer, 0 , 9);
+    
+    //Pointer to a pointer that stores the address to the buffer pointer
+    char** ptr = &buffer;
+
+    //Free
+    delete[] buffer;
+
+    return 0;
+}
