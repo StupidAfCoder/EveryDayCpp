c9a5b75c4a1ff97da2b72e04075a25230bb2d1b8
Yash Malik <132210135+StupidAfCoder@users.noreply.github.com>
2025-10-23 20:29:00 +0530

    SimpleInterface

--- Classes/Interfaces/interface.cpp
+++ Classes/Interfaces/interface.cpp
@@ -1,7 +1,51 @@
 #include<iostream>
+#include<string>
+//This has become an interface that needs to have all it's method implemented for the class extending it to use!!!!
+class Interface {
+    public:
+        virtual std::string displayClass() = 0;
+};
+
+class Player : public Interface {
+    private:
+    float X = 0.0f , Y = 0.0f;
+    public:
+        float getX(){
+            return X;
+        }
+
+        float getY(){
+            return Y;
+        }
+
+        void SetXY(float x , float y){
+            X = x;
+            Y = y;
+            std::cout << "The value has been set!! " << X << " " <<  Y << std::endl;
+        }
+
+        std::string displayClass() override {
+            return "Player";
+        }
+};
+
+class Enemy : public Interface {
+    public:
+        std::string displayClass() override {
+            return "Enemy";
+        }
+};
+
+void PrintClass(Interface* obj){
+    std::cout << obj -> displayClass() << std::endl;
+}
 
 int main(){
-    int number{6};
-    std::cout << "The number is: " << number << std::endl;
+    Player* player1 = new Player();
+    Enemy* enemy1 = new Enemy();
+    PrintClass(player1);
+    PrintClass(enemy1);
+
+    player1 -> SetXY(1.2F,1.3F);
     return 0;
 }
