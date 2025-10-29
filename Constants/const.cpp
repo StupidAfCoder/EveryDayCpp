f49060973de304d942724924c81c5fd77f9ffd3f
Yash Malik <132210135+StupidAfCoder@users.noreply.github.com>
2025-10-29 15:19:13 +0530

    Mutable!!

--- Constants/const.cpp
+++ Constants/const.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
 
+class Entity{
+    private:
+        int* M_X , *M_Y;
+        mutable int var; //These variables can be modified inside the const method!!
+    public:
+        int* getX() const{
+            //You can't change the value of the class member variables
+            return M_X;
+            var += 2;
+        }
+        //This means the pointer that is being given out can't be chaned by reference or value and the method can't modify the constant!!
+        const int* const getY() const {
+            return M_Y;
+        }
+};
+
+
+
 int main(){
     //const helps firstly by making an constant variable that cannot be changed!!
     const int MAX_VALUE = 78;
@@ -7,12 +25,12 @@ int main(){
     //For any pointer you can change the value of the pointer or the address of the pointer
     int a = 56;
     int* p_a = &a;
-    std::cout << "Before changing the value" << a << std::endl;
+    std::cout << "Before changing the value " << a << std::endl;
     *p_a = 545;
-    std::cout << "After changing the value" << a << std::endl;
+    std::cout << "After changing the value " << a << std::endl;
     int b = 78;
     p_a = &b;
-    std::cout << "Changing the address of the pointer" << *p_a << std::endl;
+    std::cout << "Changing the address of the pointer " << *p_a << std::endl;
 
     //Now if the pointer is constant!!
     // Pointer-to-const: you cannot change the value pointed-to, but you can change the pointer
@@ -26,5 +44,8 @@ int main(){
     int* const const_ptr = &a; // pointer is const, pointee (a) is mutable
     // const_ptr = &b; // ERROR: cannot change a const pointer's address
     *const_ptr = 90; // OK: modifies 'a'ṇṇ
+
+    //If you want both to be non changeable
+    const int* const const_ptr = &a;
     return 0;
 }
