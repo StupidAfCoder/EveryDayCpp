27e31325e1d328de008ffd261c300637892f4b46
StupidAfCoder <malikyash2007@gmail.com>
2025-11-09 01:29:57 +0530

    More Ternary

--- Ternary/ternary.cpp
+++ Ternary/ternary.cpp
@@ -1,3 +1,4 @@
+#include <exception>
 #include<iostream>
 #include<string>
 
@@ -6,7 +7,17 @@ int main(){
     std::string name;
     std::cin >> name;
     bool check{};
+    //Basic
     check = name == "Yash" ? true : false;
-    std::cout << check;
+    std::cout << check << std::endl;
+    //Some Thing worse
+    int score;
+    std::cout << "Give me a defined score!" << std::endl;
+    std::cin >> score;
+    int current_score;
+    std::cout << "Give me the current score" << std::endl;
+    std::cin >> current_score;
+    int get_score = current_score > score && score > 0 ? current_score < 100 ? 100 - current_score : current_score - score : 0;
+    std::cout << get_score << std::endl;
     return 0;
 }
